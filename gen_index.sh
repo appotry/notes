f0fa5ea242b9997b787eee10e0a2df3138243774
yangjinjie <51474159@qq.com>
2018-03-08 17:54:05 +0800

    modified: gen_index.sh

--- gen_index.sh
+++ gen_index.sh
@@ -10,7 +10,7 @@ dir=`find . -type d |egrep -v "\.git|assets|_book|node_modules|\.vscode|^\.$"|se
 for path in $dir
 do
   echo $path
-  title=`basename "$path"|gsed "s#^[0-9].*-##g"|sed 's#@# #g'`
+  title=`basename "$path"|sed "s#^[0-9].*-##g"|sed 's#@# #g'`
   echo $title
   index_file=`echo "${path}/index.rst" | sed 's#@#\\ #g'`
   echo $index_file
