a2f60cf335eb094b09a0d8a3f7f1492819c8ce69
yangjinjie <51474159@qq.com>
2018-03-09 08:39:57 +0800

    modified: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -63,17 +63,15 @@ pre_build(){
 
 rst(){
     cd ..
-    git clone --depth=2 --branch=master https://github.com/yangjinjie/notes.git notes_rst
-    echo $PWD
+    git clone https://github.com/yangjinjie/notes.git notes_rst
     cd notes_rst
-    cat .git/config
-    git checkout -b
+    echo $PWD
     git checkout -b rst origin/rst
-
     git merge -m "Merge branch 'master' into rst" master
-    bash gen_index.sh && git commit -a "gen index & md2rst"
+    bash gen_index.sh && git add . && git commit -m "gen index & md2rst"
+    git push "https://${GH_TOKEN}@github.com/yangjinjie/notes.git" rst:rst
+
     exit
-    git push
     cd ../notes
 }
 
