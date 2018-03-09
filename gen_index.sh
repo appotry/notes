5ebcbbbb9653abee06340e7627e9492c34fabbd6
yangjinjie <51474159@qq.com>
2018-03-09 08:48:27 +0800

    modified: gen_index.sh

--- gen_index.sh
+++ gen_index.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # md2rst
-
 find . -type f -name "*.md"|awk -F "." '{print "pandoc -f markdown -t rst \""$0"\" -o \"." $2".rst\""}'|bash
 
 # gen index
@@ -9,11 +8,11 @@ dir=`find . -type d |egrep -v "\.git|assets|_book|node_modules|\.vscode|^\.$"|se
 
 for path in $dir
 do
-  echo $path
+  # echo $path
   title=`basename "$path"|sed "s#^[0-9].*-##g"|sed 's#@# #g'`
-  echo $title
+  # echo $title
   index_file=`echo "${path}/index.rst" | sed 's#@#\\ #g'`
-  echo $index_file
+  # echo $index_file
   echo -e "$title
 ==============================
 
